202599ac895d7e94de823faf5b36dabcdd394aad
Fady Sameh Farahat Abdullah <132570927+fin84@users.noreply.github.com>
2024-06-22 14:29:23 +0300

    main.py

--- main.py
+++ main.py
@@ -1,14 +1,34 @@
-from typing import Optional
-
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File, HTTPException
+from fastapi.responses import JSONResponse
+import google.generativeai as genai
+from PIL import Image
+import io
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+genai.configure(api_key="AIzaSyDsew44Tp85JUa_w980KOLojgVxCJjEJHw")
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
+    try:
+     
+        image = Image.open(io.BytesIO(await file.read()))
+    except Exception as e:
+        raise HTTPException(status_code=400, detail="Invalid image file")
+
+   
+    image_bytes = io.BytesIO()
+    image.save(image_bytes, format=image.format)
+    image_bytes = image_bytes.getvalue()
+
+  
+    model = genai.GenerativeModel('gemini-1.5-flash')
+
+    
+    response = model.generate_content(["Determine the type of image if it is (CT), (MRI), or (X-ray) or (not).", image_bytes], stream=True)
+    response.resolve()
+
+    result = response.text
+
+    return JSONResponse(content={"classification": result})
